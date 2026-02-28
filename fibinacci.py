6c8655b82c1701f2630ecbee5d706d980de5e691
Anshu-git22 <Anshu-git22>
2026-02-28 12:52:38 +0530

    28-02-2026

---
+++ fibinacci.py
@@ -0,0 +1,6 @@
+n=int(input("Enter a value of n:"))
+a,b=0,1
+print(a,end="")
+while b<n:
+    print(b<n)
+    a,b=b,a+b
--- pyramid.py
+++ pyramid.py
@@ -1,4 +1,6 @@
-for i in range(1,5):
-    for j in range(1, i+1):
-     print(i,end="")
+for i in range(1, 6):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
     print()
